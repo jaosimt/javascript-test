f1603a9a8936c267e09ffcfa313b938f2b2023f8
sim <sim@metron.io>
2019-05-03 19:36:21 +0800

    part 3; a build from a separate react-user-app

---
+++ part3/react-user-app/precache-manifest.8c4396f48e6c59f93ef5abe8fbee78d3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b447ad317d44768d75037c0d24b88a",
+    "url": "/react-user-app/index.html"
+  },
+  {
+    "revision": "356069b4ed874df48d4b",
+    "url": "/react-user-app/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "dbd9b44e6802d486cc63",
+    "url": "/react-user-app/static/css/main.991412b1.chunk.css"
+  },
+  {
+    "revision": "356069b4ed874df48d4b",
+    "url": "/react-user-app/static/js/2.90997efb.chunk.js"
+  },
+  {
+    "revision": "dbd9b44e6802d486cc63",
+    "url": "/react-user-app/static/js/main.26b697e2.chunk.js"
+  },
+  {
+    "revision": "b23fec7e15fa10516294",
+    "url": "/react-user-app/static/js/runtime~main.8f1cd99c.js"
+  },
+  {
+    "revision": "a0e1650476464fe83258fa544557a82b",
+    "url": "/react-user-app/static/media/FEMap.a0e16504.png"
+  },
+  {
+    "revision": "e7baf491c42a4f40f1bce00a76256a5c",
+    "url": "/react-user-app/static/media/nav-last.e7baf491.png"
+  },
+  {
+    "revision": "03d394f3a2c4b34bd76fdc2378320763",
+    "url": "/react-user-app/static/media/nav-next.03d394f3.png"
+  },
+  {
+    "revision": "fe905b48ca9573d51d13ab0dd4033b2e",
+    "url": "/react-user-app/static/media/noimage.fe905b48.png"
+  },
+  {
+    "revision": "432421d2b7fe256122bd58156765877c",
+    "url": "/react-user-app/static/media/reset.432421d2.png"
+  }
+]);
